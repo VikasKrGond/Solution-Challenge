36b3357b47610ef7e076a72393a015db332ab50c
Vikas Kumar Gond <76789978+VikasKrGond@users.noreply.github.com>
2021-03-17 20:38:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightnav=document.querySelector('.rightnav')
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('.v-class-resp');
+    nav-list.classList.toggle('.v-class-resp');
+})
